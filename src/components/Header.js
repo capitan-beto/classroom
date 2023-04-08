912c85591ce3530da31988b2f59e3719cc28e89d
Otroletravaladna <crnana98@gmail.com>
2023-04-07 21:25:12 -0300

    Add header nav menu

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeaderMenu from './HeaderMenu'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Daniel Verra</h1>
+        <HeaderMenu />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/HeaderMenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HeaderMenu = () => {
+  return (
+    <div>
+        <ul>
+            <li><a href="">Percusión Latinoamericana</a></li>
+            <li><a href="">Folclore</a></li>
+            <li><a href="">Coro Pablo VI</a></li>
+            <li><a href="">Educación Vocal I</a></li>
+            <li><a href="">Añadir archivo</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default HeaderMenu
