cc4b6f48fcddce21fb659dfda55305fafb819cbb
Otroletravaladna <crnana98@gmail.com>
2023-04-19 02:22:20 -0300

    Add firestore read function

--- src/components/subjects/Percusion.js
+++ src/components/subjects/Percusion.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { getDocs, collection} from "firebase/firestore/lite";
+import { db } from "../base";
 
 const Percusion = () => {
+    const [data, setData] = useState("");
+
+    const GetPercusionData = async () => {
+        const querySnap = await getDocs(collection(db, "percusionlat")) ;
+        return querySnap.forEach((doc) => {
+            const item = doc.data();
+            setData(curr => {
+                return {...curr, item};
+            })
+        })
+    }
+
+    GetPercusionData();
+
+    console.log(data);
+
   return (
-    <div>Hi mom!</div>
+    <div></div>
   )
 }
 
