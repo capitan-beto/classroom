822d83375ce9fae86abd0f204ebb691a14d2d5ad
Otroletravaladna <crnana98@gmail.com>
2023-04-21 22:34:02 -0300

    Edit admin file displaey layout

--- src/components/subjects/FileDisplayAdmin.js
+++ src/components/subjects/FileDisplayAdmin.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-const FileDisplayAdmin = () => {
+const FileDisplayAdmin = ({ files }) => {
+  const [title, setTitle] = useState("");
+  const [titleBtn, setTitleBtn] = useState("Edit");
+
+  const Title = (item) => {
+    if (title !== item.id) return <Modal.Title>{item.id}</Modal.Title>;
+    else return <Form.Group> 
+      <Form.Control type='text'/>
+    </Form.Group> 
+  }
+
+  const handleTitle = (title) => {
+    setTitle(title);
+    if (titleBtn === "Edit") setTitleBtn("Save");
+    else setTitleBtn("Edit");
+  }
+
   return (
-    <div>FileDisplay-admin</div>
+    <div>
+      {files.map(({ title, desc, path }) => {
+        return <div
+            className='modal show'
+            style={{ display: "block", position: "initial" }}
+            key={title}
+          >
+            <Modal.Dialog>
+              <Modal.Header>
+                <Title id={title}/>
+                <Button variant="outline-secondary" onClick={() => handleTitle(title)}>
+                    {titleBtn}
+                </Button>
+              </Modal.Header>
+              <Modal.Body>
+                <h2>{desc}</h2>
+                <p>
+                  <a href={path} target='_blank'>
+                    <Button>Link</Button>
+                  </a>
+                </p>
+              </Modal.Body>
+            </Modal.Dialog>
+          </div>
+      })}
+    </div>
   )
 }
 
