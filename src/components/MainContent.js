92e0eec0e5513d9f98a45a0a2339d4f5ba62f623
Otroletravaladna <crnana98@gmail.com>
2023-04-25 05:26:09 -0300

    Refactor code

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,8 +4,8 @@ const MainContent = () => {
   return (
     <div>
         <h1>Como encuentro mis archivos?</h1>
-        <h2>Muy fácil, primero hacé click en la pestaña de tu cátedra/institución</h2>
-        <h2>Navegá a través del feed y descargá el archivo que necesites!</h2>
+        <h3>Muy fácil, primero hacé click en la pestaña de tu cátedra/institución</h3>
+        <h3>Navegá a través del feed y descargá el archivo que necesites!</h3>
     </div>
   )
 }
