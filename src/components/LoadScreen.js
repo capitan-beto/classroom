51f029fbd1745a96ffa4b7583cdb9d3fd867981f
Otroletravaladna <crnana98@gmail.com>
2023-04-26 21:30:33 -0300

    Edit layout

--- src/components/LoadScreen.js
+++ src/components/LoadScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Spinner from "react-bootstrap/Spinner";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const LoadScreen = () => {
   return (
-    <h1>Cargando...</h1>
+    <Container style={{ height: "100%"}}>
+      <Row>
+        <Col>
+          <Spinner animation="grow" variant="success" className='m-auto'>
+              <span className='visually-hidden'>Cargando...</span>
+          </Spinner>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
