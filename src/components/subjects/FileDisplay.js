0799dac241d63ea2ab6a18b18f186ad9d893ecb9
Otroletravaladna <crnana98@gmail.com>
2023-04-26 21:31:10 -0300

    Edit loadScreen container

--- src/components/subjects/FileDisplay.js
+++ src/components/subjects/FileDisplay.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import LoadScreen from '../LoadScreen';
 
 const FileDisplay = ({ files }) => {
 
   return (
-    files && <div>
-      {files.map(({ title, desc, path }) => {
-        return <div
-            className='modal show'
-            style={{ display: "block", position: "initial" }}
-            key={title}
-          >
-            <Modal.Dialog>
-              <Modal.Header>
-                <Modal.Title>{title}</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <h2>{desc}</h2>
-                <p>
-                  <a href={path} target='_blank' rel='noreferrer'>
-                    <Button>Abrir en nueva pestaña</Button>
-                  </a>
-                </p>
-              </Modal.Body>
-            </Modal.Dialog>
-          </div>
-      })}
+    files ?
+    <div>
+     {files.map(({ title, desc, path }) => {
+       return <div
+           className='modal show'
+           style={{ display: "block", position: "initial" }}
+           key={title}
+         >
+           <Modal.Dialog>
+             <Modal.Header>
+               <Modal.Title>{title}</Modal.Title>
+             </Modal.Header>
+             <Modal.Body>
+               <h2>{desc}</h2>
+               <p>
+                 <a href={path} target='_blank' rel='noreferrer'>
+                   <Button>Abrir en nueva pestaña</Button>
+                 </a>
+               </p>
+             </Modal.Body>
+           </Modal.Dialog>
+         </div>
+     })}
+    </div>
+      :
+    <div style={{ 
+      height: "100%",
+      display: "grid",
+      placeContent:"center"}}
+    >
+     <LoadScreen/>
     </div>
   )
 }
