d7e6ad30a6d34cdf666057eb03498fc023d95d49
Otroletravaladna <crnana98@gmail.com>
2023-05-25 00:11:31 -0300

    Move LoginError to services folder

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
-import LoginError from './LoginError';
+import LoginError from './services/LoginError';
 import Logout from './Logout';
 import AddFile from './AddFile';
 import { signInWithEmailAndPassword } from "firebase/auth";
