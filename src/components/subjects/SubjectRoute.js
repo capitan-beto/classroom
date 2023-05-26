bb38bfe1c080d42e23ee219edcccc9141caf4046
Otroletravaladna <crnana98@gmail.com>
2023-05-25 22:23:01 -0300

    Create file to replace each subject file

---
+++ src/components/subjects/SubjectRoute.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import {  collection, onSnapshot, query} from "firebase/firestore";
+import { db } from "../services/base";
+import FileDisplayAdmin from '../subjects/FileDisplayAdmin';
+import FileDisplay from '../subjects/FileDisplay';
+
+
+const SubjectRoute = ({ logState, subject }) => {
+  const [files, setFiles] = useState();
+
+  function getData() {
+    const q = query(collection(db, subject));
+    const unsubs = onSnapshot(q, (querySnapshot) => {
+      const data = [];
+      querySnapshot.forEach((doc) => {
+        const item = doc.data();
+        item.id = doc.id;
+        data.push(item);
+      })
+      setFiles(data);
+    })
+  }
+
+  useEffect(() => {
+    let ignore = false;
+    getData();
+    return () => ignore = true;
+  }, []);
+
+  return (
+    logState ?
+      <FileDisplayAdmin files={files} subject={subject} />
+    :
+      <FileDisplay files={files}/>
+  )
+}
+
+export default SubjectRoute;
