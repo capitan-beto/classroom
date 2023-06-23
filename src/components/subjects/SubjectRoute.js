e7bce49009290ae570203c06b387bb4c1edbb5b5
Otroletravaladna <crnana98@gmail.com>
2023-06-22 23:18:11 -0300

    refactor: Clean code and imports

--- src/components/subjects/SubjectRoute.js
+++ src/components/subjects/SubjectRoute.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import {  collection, onSnapshot, query} from "firebase/firestore";
-import { db } from "../../services/base";
+import React from 'react';
 import FileDisplayAdmin from '../subjects/FileDisplayAdmin';
 import FileDisplay from '../subjects/FileDisplay';
 import { useFetchItems } from '../../hooks/useFetchItems';
@@ -8,26 +6,6 @@ import { useFetchItems } from '../../hooks/useFetchItems';
 
 const SubjectRoute = ({ logState, subject }) => {
   const { files } = useFetchItems(subject);
-  // const [files, setFiles] = useState();
-
-  // function getData() {
-  //   const q = query(collection(db, subject));
-  //   const unsubs = onSnapshot(q, (querySnapshot) => {
-  //     const data = [];
-  //     querySnapshot.forEach((doc) => {
-  //       const item = doc.data();
-  //       item.id = doc.id;
-  //       data.push(item);
-  //     })
-  //     setFiles(data);
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   let ignore = false;
-  //   getData();
-  //   return () => ignore = true;
-  // }, []);
 
   return (
     logState ?
