c0004c82dd9db5a9d1fe686e12ebd52980f1f05f
Otroletravaladna <crnana98@gmail.com>
2023-06-22 23:58:25 -0300

    refactor: Clean imports

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Logout from '../assets/Logout';
 import AddFile from './AddFile';
 import { signInWithEmailAndPassword } from "firebase/auth";
